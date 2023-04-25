20fa0f9df299f1abdcffbd4bdfaa98e47127bb4b
phdaccache <pedrodaccache@gmail.com>
2023-04-25 08:27:45 -0300

    Sinistro

--- Lab03/src/pacote/Sinistro.java
+++ Lab03/src/pacote/Sinistro.java
@@ -1,5 +1,95 @@
 package pacote;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.StringJoiner;
+
 public class Sinistro {
+    private static int contador_id = 0;
+    private final int ID;
+    LocalDate data;
+    private String endereco;
+    private Seguradora seguradora;
+    private Veiculo veiculo;
+    private Cliente cliente;
+
+    public Sinistro(String data, String endereco, Seguradora seguradora, Veiculo veiculo, Cliente cliente) {
+        this.ID = gerarId();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        this.data = LocalDate.parse(data, dtf);
+        
+        this.endereco = endereco;
+        this.seguradora = seguradora;
+        this.veiculo = veiculo;
+        this.cliente = cliente;
+    }
     
+    @Override
+    public String toString() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        StringJoiner joiner = new StringJoiner("\n");
+        joiner.add("ID:" + getID());
+
+		String dataString = getData().format(dtf);
+        joiner.add("Data :" + dataString);
+
+        joiner.add("Endereco:" + getEndereco());
+        joiner.add("Seguradora:" + getSeguradora().toString());
+        joiner.add("Veiculo:" + getVeiculo().toString());
+        joiner.add("Cliente:" + getCliente().toString());
+
+        return joiner.toString();
+    }
+
+    // Função que gera ID único (começa em 0 e vai acumulando a cada novo objeto criado).
+    public int gerarId(){
+        contador_id += 1;
+        return contador_id;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public LocalDate getData() {
+        return this.data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Seguradora getSeguradora() {
+        return this.seguradora;
+    }
+
+    public void setSeguradora(Seguradora seguradora) {
+        this.seguradora = seguradora;
+    }
+
+    public Veiculo getVeiculo() {
+        return this.veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
