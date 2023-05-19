fa5724944fb6bb6728612c817796c247482b8790
phdaccache <pedrodaccache@gmail.com>
2023-05-19 19:07:09 -0300

    prototipo Validacao

--- Lab04/src/pacote/Validacao.java
+++ Lab04/src/pacote/Validacao.java
@@ -1,5 +1,23 @@
 package pacote;
 
 public class Validacao {
-    
+    // Validar CPF
+    public static boolean validarCPF(String cpf) {
+        return true;
+    }
+
+    // Validar CNPJ
+    public static boolean validarCNPJ(String cnpj) {
+        return true;
+    }
+
+    // Validar nome
+    public static boolean validarNome(String nome) {
+        return true;
+    }
+
+    // Validar data
+    public static boolean validarData() {
+        return true;
+    }
 }
