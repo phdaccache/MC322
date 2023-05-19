1d7227275d21fbab938e2225f2ccc685699c5972
phdaccache <pedrodaccache@gmail.com>
2023-05-19 19:12:27 -0300

    implementação CalcSeguro

--- Lab04/src/pacote/CalcSeguro.java
+++ Lab04/src/pacote/CalcSeguro.java
@@ -1,5 +1,28 @@
 package pacote;
 
+/* enum para calcular o valor do seguro
+ * A primeira constante e um valor base
+ * As proximas constantes sao fatores que variam com a idade
+ */
 public enum CalcSeguro {
-    
+    VALOR_BASE(100),
+    // Idade entre 18 e 30
+    FATOR_18_30(1.2),
+    // Idade entre 30 e 60
+    FATOR_30_60(1.0),
+    // Idade entre 60 e 90
+    FATOR_60_90(1.5);
+
+    // Atributo
+    private double valor;
+
+    // Construtor
+    CalcSeguro(double valor) {
+        this.valor = valor;
+    }
+
+    // Getter
+    public double getValor() {
+        return valor;
+    }
 }
