518c502882211760b0ff2d3a6b13a19c50c15224
phdaccache <pedrodaccache@gmail.com>
2023-06-08 15:08:11 -0300

    funcoes Frota

--- Lab05/src/pacote/Frota.java
+++ Lab05/src/pacote/Frota.java
@@ -1,6 +1,7 @@
 package pacote;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class Frota {
@@ -30,6 +31,82 @@ public class Frota {
 
         return joiner.toString();
     }
+
+    // Listar todos os veiculos
+    public void listarVeiculos() {
+        // Caso em que nao ha veiculos cadastrados
+        if (listaVeiculos.isEmpty()) {
+            System.out.println("Nao ha veiculos cadastrados.");
+            return;
+        }
+
+        System.out.println("Veiculos:");
+        // Iterando sobre os veiculos
+        for (Veiculo veiculo : listaVeiculos) {
+            System.out.println("---------------------------------------------");
+            System.out.println(veiculo);
+        }
+        System.out.println("---------------------------------------------");
+    }
+
+    // Cadastrar novo veiculo automatico
+    public void cadastrarVeiculo(Veiculo veiculo) {
+        // Caso em que a placa passada e invalida
+        if (!Validacao.validarPlaca(veiculo.getPlaca())) {
+            System.out.println("Placa invalida. Nao foi possivel cadastrar o veiculo.");
+            return;
+        }
+
+        listaVeiculos.add(veiculo); // Veiculo adicionado
+        System.out.println("Veiculo cadastrado!");
+    }
+
+    // Cadastrar novo veiculo com scanner
+    public void cadastrarVeiculo(Scanner scanner) {
+        System.out.print("Digite a placa: ");
+        String placa = scanner.nextLine();
+        System.out.print("Digite a marca: ");
+        String marca = scanner.nextLine();
+        System.out.print("Digite o modelo: ");
+        String modelo = scanner.nextLine();
+        System.out.print("Digite o ano de fabricacao: ");
+        int ano = scanner.nextInt();
+
+        Veiculo veiculo = new Veiculo(placa, marca, modelo, ano);
+
+        cadastrarVeiculo(veiculo);
+    }
+
+    // Excluir veiculo automatico
+    public void excluirVeiculo(Veiculo veiculo) {
+        String placa = veiculo.getPlaca();
+
+        // Checar se o veiculo existe antes de excluir
+        if (listaVeiculos.contains(veiculo)) {
+            listaVeiculos.remove(veiculo);
+            System.out.printf("Veiculo de placa %s removido!\n", placa);
+            return;
+        }
+
+        System.out.printf("Veiculo invalido. Nao foi possivel remover o veiculo de placa %s.\n", placa);
+    }
+
+    // Excluir veiculo com scanner
+    public void excluirVeiculo(Scanner scanner) {
+        System.out.print("Insira a placa do veiculo que deseja excluir: ");
+        String placa = scanner.nextLine();
+
+        for (Veiculo veiculo: listaVeiculos) {
+            if (veiculo.getPlaca().equals(placa)) {
+                listaVeiculos.remove(veiculo);
+                System.out.printf("Veiculo de placa %s removido!\n", placa);
+                return;
+            }
+        }
+
+        System.out.printf("Veiculo invalido. Nao foi possivel remover o veiculo de placa %s.\n", placa);
+    }
+    
 
     // Getters e Setters
     public int getId() {
