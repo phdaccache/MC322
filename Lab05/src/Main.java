60daaf4b89825e0ae10d916784e03727ff12e66e
phdaccache <pedrodaccache@gmail.com>
2023-06-09 19:21:29 -0300

    menu na main

--- Lab05/src/Main.java
+++ Lab05/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
+import menu.Menu;
+import menu.MenuOperacoes;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        /* MENU INTERATIVO */
+
+        Scanner scanner = new Scanner(System.in);
+        Menu menu = new Menu(scanner);
+        MenuOperacoes option;
+
+        // Executa menu (imprime, recebe a opcao e executa a opcao) de acordo com a opcao passada
+        do {
+            menu.showMenu();
+            option = menu.readOptionMenu();
+            menu.runMenuOption(option);
+        } while (option != MenuOperacoes.SAIR);
+
+        scanner.close();
     }
 }
